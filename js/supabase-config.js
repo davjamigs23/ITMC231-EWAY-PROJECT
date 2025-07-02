0973da13b11d2cb5ed00826ddf8e7e2114b5ba26
David James B. Ignacio <djignacio@gbox.adnu.edu.ph>
2025-07-03 01:51:54 +0800

    1

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,9 +1,9 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const SUPABASE_URL = 'https://lqytdmbpaknugrrjoylh.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxeXRkbWJwYWtudWdycmpveWxoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzODM3MTQsImV4cCI6MjA2Njk1OTcxNH0.B9AndZdM-_2_ojsbqSC-p7hboIN0XKsrs8TBEnDO3lY';
+
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  
   auth: {
     autoRefreshToken: true,
     persistSession: true,
